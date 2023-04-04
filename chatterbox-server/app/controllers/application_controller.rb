2fcccb7eb7605a3593add3285d8699f3bb81c34c
Pauline Muigai <wanjirumuigai@gmail.com>
2023-04-04 18:32:05 +0300

    completed the POST route

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add routes
-  
+  get "/messages" do
+    messages = Message.all.sort_by(&:created_at)
+    messages.to_json
+  end
+
+  post "/messages" do
+    message = Message.create(body: params[:body], username: params[:username])
+    message.to_json
+  end
 end
